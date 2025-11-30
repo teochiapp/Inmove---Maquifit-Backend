b0e6ccd1a825a04683fd30b5cbacf06090e91932
Teo Chiappero <teochiapps@gmail.com>
2025-11-30 15:07:54 -0300

    exif changes

--- src/extensions/upload/strapi-server.js
+++ src/extensions/upload/strapi-server.js
@@ -26,66 +26,106 @@ const ensureWorkingDirectory = async (file) => {
   return workingDir;
 };
 
-const normalizeImageOrientation = async (file) => {
-  const imageManipulationService = strapi.plugin('upload').service('image-manipulation');
+const shouldSkipRotation = (orientation, width, height) => {
+  if (!orientation || !width || !height) {
+    return false;
+  }
 
-  if (!(await imageManipulationService.isImage(file))) {
+  // Orientations 6 and 8 expect the stored pixels to be landscape (width > height).
+  // If the pixels are already portrait (height >= width), skip rotation.
+  if ((orientation === 6 || orientation === 8) && height >= width) {
+    return true;
+  }
+
+  // Orientations 3 and 4 expect pixels to be upside-down.
+  // If dimensions suggest it's already correct, skip.
+  if ((orientation === 3 || orientation === 4) && width >= height) {
+    return true;
+  }
+
+  return false;
+};
+
+const createOrientationNormalizer = (imageManipulationService) => {
+  const { isImage } = imageManipulationService;
+
+  return async (file) => {
+    if (!(await isImage(file))) {
+      return file;
+    }
+
+    const hasFilePath = Boolean(file.filepath);
+    let metadata;
+    let inputBuffer;
+
+    if (hasFilePath) {
+      metadata = await sharp(file.filepath).metadata();
+    } else {
+      inputBuffer = await streamToBuffer(file.getStream());
+      metadata = await sharp(inputBuffer).metadata();
+    }
+
+    strapi.log.info(
+      `[upload] Image metadata: ${file.name || file.hash}${file.ext || ''} - orientation: ${metadata?.orientation}, width: ${metadata?.width}, height: ${metadata?.height}`
+    );
+
+    if (
+      !metadata?.orientation ||
+      metadata.orientation === 1 ||
+      shouldSkipRotation(metadata.orientation, metadata.width, metadata.height)
+    ) {
+      if (metadata?.orientation && shouldSkipRotation(metadata.orientation, metadata.width, metadata.height)) {
+        strapi.log.info(
+          `[upload] Skipped rotation for ${file.name || file.hash}${
+            file.ext || ''
+          } - pixels already match orientation ${metadata.orientation}`
+        );
+      }
+      return file;
+    }
+
+    const workingDirectory = await ensureWorkingDirectory(file);
+    const orientedFileName = `oriented-${file.hash}${file.ext || ''}`;
+    const orientedFilePath = path.join(workingDirectory, orientedFileName);
+
+    const transformer = hasFilePath ? sharp(file.filepath) : sharp(inputBuffer);
+    const result = await transformer
+      .rotate()
+      .withMetadata({ orientation: 1 })
+      .toFile(orientedFilePath);
+
+    const sizeInBytes = result.size ?? 0;
+
+    strapi.log.info(
+      `[upload] Rotated ${file.name || file.hash}${file.ext || ''} from EXIF orientation ${metadata.orientation} - new dimensions: ${result.width}x${result.height}`
+    );
+
+    file.filepath = orientedFilePath;
+    file.getStream = () => fse.createReadStream(orientedFilePath);
+    file.buffer = await fse.readFile(orientedFilePath);
+    file.size = bytesToKbytes(sizeInBytes);
+    file.sizeInBytes = sizeInBytes;
+
+    if (result.width && result.height) {
+      file.width = result.width;
+      file.height = result.height;
+    }
+
     return file;
-  }
-
-  const hasFilePath = Boolean(file.filepath);
-  let metadata;
-  let inputBuffer;
-
-  if (hasFilePath) {
-    metadata = await sharp(file.filepath).metadata();
-  } else {
-    inputBuffer = await streamToBuffer(file.getStream());
-    metadata = await sharp(inputBuffer).metadata();
-  }
-
-  if (!metadata?.orientation || metadata.orientation === 1) {
-    return file;
-  }
-
-  const workingDirectory = await ensureWorkingDirectory(file);
-  const orientedFileName = `oriented-${file.hash}${file.ext || ''}`;
-  const orientedFilePath = path.join(workingDirectory, orientedFileName);
-
-  const transformer = hasFilePath ? sharp(file.filepath) : sharp(inputBuffer);
-  const result = await transformer
-    .rotate()
-    .withMetadata({ orientation: 1 })
-    .toFile(orientedFilePath);
-
-  const sizeInBytes = result.size ?? 0;
-
-  strapi.log.info(
-    `[upload] normalizeImageOrientation rotated ${file.name || file.hash}${file.ext || ''} from EXIF orientation ${metadata.orientation}`
-  );
-
-  file.filepath = orientedFilePath;
-  file.getStream = () => fse.createReadStream(orientedFilePath);
-  file.size = bytesToKbytes(sizeInBytes);
-  file.sizeInBytes = sizeInBytes;
-
-  if (result.width && result.height) {
-    file.width = result.width;
-    file.height = result.height;
-  }
-
-  return file;
+  };
 };
 
 module.exports = (plugin) => {
-  const originalUploadFileAndPersist = plugin.services.upload.uploadFileAndPersist;
+  const imageManipulationService = plugin.services['image-manipulation'];
+  const originalOptimize = imageManipulationService.optimize;
+  const normalizeImageOrientation = createOrientationNormalizer(imageManipulationService);
 
-  plugin.services.upload.uploadFileAndPersist = async function uploadFileAndPersist(fileData, opts) {
-    const normalizedFile = await normalizeImageOrientation(fileData);
-    return originalUploadFileAndPersist.call(this, normalizedFile, opts);
+  imageManipulationService.optimize = async function optimizedWithOrientation(file) {
+    const orientedFile = await normalizeImageOrientation(file);
+    return originalOptimize.call(this, orientedFile);
   };
 
-  plugin.services.upload.normalizeImageOrientation = normalizeImageOrientation;
+  imageManipulationService.normalizeImageOrientation = normalizeImageOrientation;
 
   return plugin;
 };
